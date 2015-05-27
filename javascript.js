c0dab12af523a9af477bc9153999f17978398697
Francisco Limongi <zico@ri5.com.br>
2015-05-27 00:17:08 -0300

    finalizado, mas comproblemas no espaços

--- javascript.js
+++ javascript.js
@@ -3,3 +3,36 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+function requestEpisode(episode_id){
+  var getEpisode = new XMLHttpRequest();
+  getEpisode.onreadystatechange = callbackGetAllFilms;
+  getEpisode.open('GET', 'https://swapi.co/api/films/'+episode_id, true);
+  getEpisode.send(null);
+}
+
+
+function callbackGetAllFilms(){
+  if(this.readyState === 4) {
+     if(this.status === 200) {
+      changeOpeningCrawl(JSON.parse(this.response).opening_crawl)
+    } else {
+      console.log('Erro ao curtir post. Código ' +
+        'da resposta HTTP: ' + this.status);
+    }
+  }
+}
+
+
+function changeOpeningCrawl(newCrawl){
+  var p = document.querySelector(".flow p");
+  newCrawl = newCrawl.replace("\n", '<br>')
+  p.innerHTML = newCrawl;
+}
+
+
+document.getElementById('movies').addEventListener('click',function(e){
+  var episodeClicked = e.target.dataset.episode;
+  requestEpisode(episodeClicked)
+});
+
